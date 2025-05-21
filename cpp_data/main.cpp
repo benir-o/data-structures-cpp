fc5ee12fd44c64c045281388d924ae52e972a681
benir-o <beniromenda@gmail.com>
2025-05-21 21:15:41 +0300

    commit 1

---
+++ cpp_data/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+//This is a test file to check if the code is working or not
+
+double addNum(int a, int b)
+{
+    return a + b;
+}
+double subtractNum(int a, int b)
+{
+    return a - b;
+}
+double multiplyNum(int a, int b)
+{
+    return a * b;
+}
+int main()
+{
+
+    cout << addNum(5, 10) << endl; // Output: 15
+    // Standard template library
+    return 0;
+}
