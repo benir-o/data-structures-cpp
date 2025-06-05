0e8fd38389dc7251931131ad35502c32ff650676
benir-o <beniromenda@gmail.com>
2025-06-05 23:47:55 +0300

    testing

---
+++ cpp_data/hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello, World!" << endl;
+    return 0;
+    // comment
+    // another comment
+    // This is a simple C++ program that prints "Hello, World!" to the console.
+}
